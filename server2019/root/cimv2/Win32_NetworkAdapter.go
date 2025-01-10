9f8e1c5fa9501f13d5763aac0f00abc091afb4d6
jason <xc9898@gmail.com>
2025-01-10 17:30:03 +0800

    修复Win32_NetworkAdapter 中错误的数据结构

--- server2019/root/cimv2/Win32_NetworkAdapter.go
+++ server2019/root/cimv2/Win32_NetworkAdapter.go
@@ -1,18 +1,20 @@
 // Copyright 2019 (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-//
 // Author:
-//      Auto Generated on 9/18/2020 using wmigen
-//      Source root.CIMV2
-//////////////////////////////////////////////
+//
+//	Auto Generated on 9/18/2020 using wmigen
+//	Source root.CIMV2
+//
+// ////////////////////////////////////////////
 package cimv2
 
 import (
+	"reflect"
+
 	"github.com/microsoft/wmi/pkg/base/query"
 	"github.com/microsoft/wmi/pkg/errors"
 	cim "github.com/microsoft/wmi/pkg/wmiinstance"
-	"reflect"
 )
 
 // Win32_NetworkAdapter struct
@@ -373,7 +375,7 @@ func (instance *Win32_NetworkAdapter) SetPropertyNetConnectionStatus(value uint1
 }
 
 // GetNetConnectionStatus gets the value of NetConnectionStatus for the instance
-func (instance *Win32_NetworkAdapter) GetPropertyNetConnectionStatus() (value uint16, err error) {
+func (instance *Win32_NetworkAdapter) GetPropertyNetConnectionStatus() (value int32, err error) {
 	retValue, err := instance.GetProperty("NetConnectionStatus")
 	if err != nil {
 		return
@@ -383,13 +385,13 @@ func (instance *Win32_NetworkAdapter) GetPropertyNetConnectionStatus() (value ui
 		return
 	}
 
-	valuetmp, ok := retValue.(uint16)
+	valuetmp, ok := retValue.(int32)
 	if !ok {
 		err = errors.Wrapf(errors.InvalidType, " uint16 is Invalid. Expected %s", reflect.TypeOf(retValue))
 		return
 	}
 
-	value = uint16(valuetmp)
+	value = int32(valuetmp)
 
 	return
 }

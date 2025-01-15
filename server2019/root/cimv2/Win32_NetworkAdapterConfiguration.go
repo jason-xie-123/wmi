d267f3944fe3de18c4a0b5c16f7ac027fd814cbe
jason <xc9898@gmail.com>
2025-01-15 14:07:47 +0800

    修复Win32_NetworkAdapterConfiguration 中错误的数据结构

--- server2019/root/cimv2/Win32_NetworkAdapterConfiguration.go
+++ server2019/root/cimv2/Win32_NetworkAdapterConfiguration.go
@@ -1,18 +1,20 @@
 // Copyright 2019 (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-//
 // Author:
-//      Auto Generated on 9/18/2020 using wmigen
-//      Source root.CIMV2
-//////////////////////////////////////////////
+//
+//	Auto Generated on 9/18/2020 using wmigen
+//	Source root.CIMV2
+//
+// ////////////////////////////////////////////
 package cimv2
 
 import (
+	"reflect"
+
 	"github.com/microsoft/wmi/pkg/base/query"
 	"github.com/microsoft/wmi/pkg/errors"
 	cim "github.com/microsoft/wmi/pkg/wmiinstance"
-	"reflect"
 )
 
 // Win32_NetworkAdapterConfiguration struct
@@ -827,7 +829,7 @@ func (instance *Win32_NetworkAdapterConfiguration) SetPropertyInterfaceIndex(val
 }
 
 // GetInterfaceIndex gets the value of InterfaceIndex for the instance
-func (instance *Win32_NetworkAdapterConfiguration) GetPropertyInterfaceIndex() (value uint32, err error) {
+func (instance *Win32_NetworkAdapterConfiguration) GetPropertyInterfaceIndex() (value int32, err error) {
 	retValue, err := instance.GetProperty("InterfaceIndex")
 	if err != nil {
 		return
@@ -837,13 +839,13 @@ func (instance *Win32_NetworkAdapterConfiguration) GetPropertyInterfaceIndex() (
 		return
 	}
 
-	valuetmp, ok := retValue.(uint32)
+	valuetmp, ok := retValue.(int32)
 	if !ok {
-		err = errors.Wrapf(errors.InvalidType, " uint32 is Invalid. Expected %s", reflect.TypeOf(retValue))
+		err = errors.Wrapf(errors.InvalidType, " int32 is Invalid. Expected %s", reflect.TypeOf(retValue))
 		return
 	}
 
-	value = uint32(valuetmp)
+	value = int32(valuetmp)
 
 	return
 }
